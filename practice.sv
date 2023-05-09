575e07637c663f92e4c34a188decf766b4bde07a
yadavashi <132988702+yadavashi@users.noreply.github.com>
2023-05-09 13:13:36 +0530

    Create practice.sv

---
+++ practice.sv
@@ -0,0 +1,9 @@
+module ass;
+  function bit [3:0] mul( bit [2:0]a,b);
+    return a*b;
+    bit [3:0] result=0;
+    initial begin 
+      result =mul(3'b111,3'b010);
+      $display ("result=%0d",result);
+    end 
+    emdmodule
